b6a7435e32a28ea6446a4d62cb15c9ca85176a80
Roman Zanko <roman.zanko.vl@gmail.com>
2026-02-02 21:28:28 +0400

    Enable both server and client code generation for gRPC in build script

--- blog-server/build.rs
+++ blog-server/build.rs
@@ -4,6 +4,8 @@ use std::path::PathBuf;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = PathBuf::from(env::var("OUT_DIR")?);
     tonic_prost_build::configure()
+        .build_server(true)
+        .build_client(true)
         .file_descriptor_set_path(out_dir.join("blog_descriptor.bin"))
         .compile_protos(&["src/proto/blog.proto"], &["src/proto"])?;
     println!("cargo:rerun-if-changed=build.rs");
